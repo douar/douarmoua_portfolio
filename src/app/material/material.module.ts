c04141545260e8aa4bfd36494c7aa0e851635e3b
douarmoua <mouadouarzong@gmail.com>
2023-07-14 20:26:54 -0500

    css and expansion panel about me

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import {MatExpansionModule} from "@angular/material/expansion";
 
 
-const MaterialComponents:any[] = []
+const MaterialComponents:any[] = [
+  MatExpansionModule
+]
 @NgModule({
   imports: [MaterialComponents],
   exports: [MaterialComponents]
