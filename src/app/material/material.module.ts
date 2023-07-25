8b0a882f8571283e41685f7d7b355ffacf4e9fd9
douarmoua <mouadouarzong@gmail.com>
2023-07-25 15:08:54 -0500

    clean code, add responsive menu

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
 
 
 const MaterialComponents:any[] = [
-  MatExpansionModule
+  MatExpansionModule,
+  MatIconModule,
+  MatMenuModule,
+  MatButtonModule
 ]
 @NgModule({
   imports: [MaterialComponents],
