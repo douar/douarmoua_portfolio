f1c8f6c115496b4afa44db32d7b0e279a9ab9239
douarmoua <mouadouarzong@gmail.com>
2024-09-17 18:55:44 -0500

    ng g about-me and container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
